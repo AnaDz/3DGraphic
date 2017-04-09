ccb6f4bcd820c235af04eb07abf8901e1634db11
Anaïs Donzeau <anais.donzeau@ensimag.grenoble-inp.fr>
2017-04-09 17:48:12 +0200

    Explosion
    Version qui marche mais bcp de code dans le main !

---
+++ include/students/ParticleExplosion.hpp
@@ -0,0 +1,34 @@
+#ifndef ParticleExplosion_HPP
+#define ParticleExplosion_HPP
+
+#include <vector>
+#include <glm/glm.hpp>
+#include "../dynamics_rendering/ParticleRenderable.hpp"
+#include "../dynamics/Particle.hpp"
+#include "../Viewer.hpp"
+#include "./../lighting/Materiable.hpp"
+
+class ParticleExplosion : public ParticleRenderable, public Materiable
+{
+public:
+    ~ParticleExplosion();
+    ParticleExplosion(ShaderProgramPtr program, const MaterialPtr& material, ParticlePtr particle);
+
+protected:
+    void do_draw();
+    void do_animate(float time);
+
+    std::vector<glm::vec3> m_positions;
+    std::vector<glm::vec4> m_colors;
+    std::vector<glm::vec3> m_normals;
+
+    unsigned int m_pBuffer;
+    unsigned int m_cBuffer;
+    unsigned int m_nBuffer;
+
+
+};
+
+typedef std::shared_ptr<ParticleExplosion> ParticleExplosionPtr;
+
+#endif
--- src/projet_skyrim2.cpp
+++ src/projet_skyrim2.cpp
@@ -51,8 +51,8 @@
 #include "../include/students/Tree.hpp"
 #include "../include/students/BonhommeDeNeige.hpp"
 #include "../include/students/GroundRenderable.hpp"
+#include "../include/students/ParticleExplosion.hpp"
 #include "../include/students/SnowballRenderable.hpp"
-
 
 void initialize_project_skyrim_2(Viewer& viewer) {
 
@@ -129,17 +129,74 @@ void initialize_project_skyrim_2(Viewer& viewer) {
     glm::mat4 scaleM(1.0);
     glm::mat4 translationM(1.0);
 
+    /* ParticleExplosion mggl*/
 
-    //Head
-    TexturedMeshRenderablePtr mesh =
-        std::make_shared<TexturedMeshRenderable>(
-            texShader, "../meshes/Maison.obj", "../textures/Cottage Texture.jpg");
-    scaleM = glm::scale(glm::mat4(1.0), glm::vec3(0.2,0.2,0.2));
-    mesh->setParentTransform(scaleM);
-     mesh->setMaterial(Material::Maison());
-    /*rotationM = glm::rotate(glm::mat4(1.0), (float)(M_PI/2.0), glm::vec3(1,0,0));
-    head->setLocalTransform(rotationM)*/
-    viewer.addRenderable(mesh);
+    system->setCollisionsDetection(true);
+
+    //Initialize the restitution coefficient for collision
+    //1.0 = full elastic response
+    //0.0 = full absorption
+    system->setRestitution(0.6f);
+
+    //Initialize a plane from 3 points and add it to the system as an obstacle
+    glm::vec3 p1(-5.0, -5.0, 0.0);
+    glm::vec3 p2(5.0, -5.0, 0.0);
+    glm::vec3 p3(5.0, 5.0, 0.0);
+    glm::vec3 p4(-5.0, 5.0, 0.0);
+    PlanePtr plane = std::make_shared<Plane>(p1, p2, p3);
+    system->addPlaneObstacle(plane);
+
+
+
+    //Create a plane renderable to display the obstacle
+    PlaneRenderablePtr planeRenderable = std::make_shared<QuadRenderable>(flatShader, p1,p2,p3,p4);
+    HierarchicalRenderable::addChild( systemRenderable, planeRenderable );
+
+    glm::vec3 px,pv;
+    float pm, pr;
+    //Particle vs Plane collision
+
+    int slices = 10;
+    int strips = 10;
+    double radius = 3.0;
+    for(int i=0; i<slices; ++i)
+    {
+        for(int j=0; j<strips; ++j)
+        {
+            double curr_theta = i*(2.0*M_PI/(double)slices);
+            double curr_phi = j*(M_PI/(double)strips);
+            /* MODULABLE */
+            /* Ce qu'on peut faire : augmenter le v ou diminuer
+            et aussi ajouter une composante pour la vitesse (genre 20 + Vx) pour que les boules soient dirigées plus vers un sens !*/
+            int v = random(2,10);
+            px = glm::vec3(0,0,3) + glm::vec3(radius*cos(curr_theta)*sin(curr_phi), radius*sin(curr_theta)*sin(curr_phi), radius*cos(curr_phi));
+            pv = glm::vec3(v*radius*cos(curr_theta)*sin(curr_phi), v*radius*sin(curr_theta)*sin(curr_phi), v*radius*cos(curr_phi));
+            pr = random(0.5,1.0);
+            pm = 10;
+            ParticlePtr particle = std::make_shared<Particle>(px, pv, pm, pr);
+            system->addParticle(particle);
+            translationM = glm::translate(glm::mat4(1.0), glm::vec3(0.0,0.0,4.0));
+            ParticleRenderablePtr particleRenderable = std::make_shared<ParticleRenderable>(flatShader, particle);
+
+            ParticleExplosionPtr expl = std::make_shared<ParticleExplosion>(phongShader, Material::Neige(), particle);
+            HierarchicalRenderable::addChild(systemRenderable, expl);
+            //particleRenderable->setParentTransform(translationM);
+          }
+        }
+        ConstantForceFieldPtr gravityForceField
+            = std::make_shared<ConstantForceField>(system->getParticles(), glm::vec3{0, 0, -10} );
+        system->addForceField(gravityForceField);
+
+
+      /*  TexturedMeshRenderablePtr mesh =
+            std::make_shared<TexturedMeshRenderable>(
+                texShader, "../meshes/Maison.obj", "../textures/Cottage Texture.jpg");
+        scaleM = glm::scale(glm::mat4(1.0), glm::vec3(0.2,0.2,0.2));
+        mesh->setParentTransform(scaleM);
+         mesh->setMaterial(Material::Maison());
+        /*rotationM = glm::rotate(glm::mat4(1.0), (float)(M_PI/2.0), glm::vec3(1,0,0));
+        head->setLocalTransform(rotationM)*/
+        //viewer.addRenderable(mesh);
 
   }
 
